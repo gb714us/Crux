ab00888e451729af879701d98f581f25f58f9bef
gb714us <cd>
2015-01-22 21:22:28 -0800

    Initial commit

---
+++ src/crux/Scanner.java
@@ -0,0 +1,197 @@
+package crux;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+
+public class Scanner implements Iterable<Token> {
+	public static String studentName = "Gabriel Blanco";
+	public static String studentID = "81443241";
+	public static String uciNetID = "gblanco1";
+	
+	private int lineNum;  // current line count
+	private int charPos;  // character offset for current line
+	private int nextChar; // contains the next char (-1 == EOF)
+	private Reader input;
+	
+	Scanner(Reader reader)
+	{
+		// TODO: initialize the Scanner
+		this.lineNum = 1;
+		this.charPos = 0;
+		this.input = reader;
+		this.nextChar = readChar();
+
+	}	
+	
+	// OPTIONAL: helper function for reading a single char from input
+	//           can be used to catch and handle any IOExceptions,
+	//           advance the charPos or lineNum, etc.
+
+
+
+	private int readChar()
+	{
+		int next = -1;   //init to -1 for EOF
+		try
+		{
+			next = input.read();
+			++charPos;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		return next;
+	}
+
+		
+
+	/* Invariants:
+	 *  1. call assumes that nextChar is already holding an unread character
+	 *  2. return leaves nextChar containing an untokenized character
+	 */
+	public Token next()
+	{
+		return generateToken();
+	}
+
+	private Token generateToken()
+	{
+		while(Character.isWhitespace(nextChar))  //Ignore whitespace
+		{
+			if (nextChar == '\n')
+			{
+				++lineNum;
+				charPos = 0;
+			}
+			nextChar = readChar();
+		}
+
+		if (nextChar == -1)   //EOF
+		{
+			try
+			{
+				input.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			return Token.EOF(lineNum, charPos);
+		}
+
+
+		StringBuilder stringBuilder;
+		switch(nextChar)  //Handle special cases  (Comments, 2-char tokens)
+		{
+			case '/':
+				stringBuilder = initialize();
+				if (nextChar == '/')
+				{
+					while(nextChar != '\n' && nextChar != -1)
+						advance(stringBuilder);
+					if (nextChar == '\n')
+						++lineNum;
+					charPos = 0;
+					nextChar = readChar();
+					return next();
+				}
+				else
+					return new Token(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			case '<':
+			case '>':
+			case '=':
+				stringBuilder = initialize();
+				if (nextChar == '=')
+					advance(stringBuilder);
+				return new Token (stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+
+			case ':':
+				stringBuilder = initialize();
+				if (nextChar == ':')
+					advance(stringBuilder);
+				return new Token (stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			case '!':
+				stringBuilder = initialize();
+				if (nextChar == '=')
+				{
+					advance(stringBuilder);
+					return new Token(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+				}
+				return Token.ERROR(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			default:
+				break;
+
+		}
+
+		if (Character.isDigit(nextChar))   //INT OR FLOAT
+		{
+			stringBuilder = initialize();
+
+			while (Character.isDigit(nextChar))
+				advance(stringBuilder);
+
+			if (nextChar == '.')
+			{
+				advance(stringBuilder);
+
+				while (Character.isDigit(nextChar))
+					advance(stringBuilder);
+
+				return Token.FLOAT(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			}
+
+			return Token.INTEGER(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+
+		}
+		else if (Character.isLetter(nextChar) || nextChar == '_')  //IDENTIFIER
+		{
+			stringBuilder = initialize();
+
+			while (Character.isLetter(nextChar) || nextChar == '_' || Character.isDigit(nextChar) )
+				advance(stringBuilder);
+
+			for (Token.Kind kind : Token.Kind.values())
+			{
+				if (kind.matches(stringBuilder.toString()))
+					return new Token(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			}
+
+			return Token.IDENTIFIER(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+		}
+		else
+		{
+			stringBuilder = initialize();
+
+			for (Token.Kind kind : Token.Kind.values())
+			{
+				if (kind.matches(stringBuilder.toString()))
+					return new Token(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+			}
+
+			return Token.ERROR(stringBuilder.toString(), lineNum, charPos - stringBuilder.length());
+		}
+
+	}
+
+	private StringBuilder initialize()
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		advance(stringBuilder);
+		return stringBuilder;
+	}
+
+	private void advance(StringBuilder s)
+	{
+		s.append((char)nextChar);
+		nextChar = readChar();
+	}
+
+	// OPTIONAL: any other methods that you find convenient for implementation or testing
+
+	@Override
+	public Iterator<Token> iterator() {
+		return null;
+	}
+}
---
+++ src/crux/Token.java
@@ -0,0 +1,193 @@
+package crux;
+
+public class Token {
+
+	public static enum Kind
+	{
+		AND("and"),
+		OR("or"),
+		NOT("not"),
+		
+		ADD("+"),
+		SUB("-"),
+		MUL("*"),
+		DIV("/"),
+		
+		IDENTIFIER(),
+		INTEGER(),
+		FLOAT(),
+		ERROR(),
+		EOF(),
+
+		OPEN_PAREN("("),
+		CLOSE_PAREN(")"),
+		OPEN_BRACE("{"),
+		CLOSE_BRACE("}"),
+		OPEN_BRACKET("["),
+		CLOSE_BRACKET("]"),
+		GREATER_EQUAL(">="),
+		LESSER_EQUAL("<="),
+		NOT_EQUAL("!="),
+		EQUAL("=="),
+		GREATER_THAN(">"),
+		LESS_THAN("<"),
+		ASSIGN("="),
+		COMMA(","),
+		SEMICOLON(";"),
+		COLON(":"),
+		CALL("::"),
+
+		LET("let"),
+		VAR("var"),
+		ARRAY("array"),
+		FUNC("func"),
+		IF("if"),
+		ELSE("else"),
+		WHILE("while"),
+		TRUE("true"),
+		FALSE("false"),
+		RETURN("return");
+		
+		private String default_lexeme;
+		
+		Kind() {default_lexeme = "";}
+		
+		Kind(String lexeme)
+		{
+			default_lexeme = lexeme;
+		}
+		
+		public boolean hasStaticLexeme()
+		{
+			return default_lexeme != "";
+		} //had to change to "" from null because of
+																		 // object never being null
+		
+		// OPTIONAL: if you wish to also make convenience functions, feel free
+		//           for example, boolean matches(String lexeme)
+		//           can report whether a Token.Kind has the given lexeme
+
+		public boolean matches(CharSequence lexeme)
+		{
+			return default_lexeme.equals(lexeme);
+		}
+	}
+	
+	private int lineNum;
+	private int charPos;
+	Kind kind;
+	private String lexeme = "";
+	
+	
+	// OPTIONAL: implement factory functions for some tokens, as you see fit
+	/**/
+	public static Token EOF(int linePos, int charPos)
+	{
+		Token token = new Token(linePos, charPos);
+		token.kind = Kind.EOF;
+		token.kind.default_lexeme = " ";
+		return token;
+	}
+
+	public static Token IDENTIFIER(String name, int linePos, int charPos)
+	{
+		Token token = new Token(linePos, charPos);
+		token.kind = Kind.IDENTIFIER;
+		token.lexeme = name;
+
+		return token;
+	}
+
+	public static Token ERROR(String error, int linePos, int charPos)
+	{
+		Token token = new Token(linePos, charPos);
+		token.kind = Kind.ERROR;
+		token.lexeme = "Unrecognized character: " + error;
+
+		return token;
+	}
+
+	public static Token INTEGER(String number, int linePos, int charPos)
+	{
+		Token token = new Token(linePos, charPos);
+		token.kind = Kind.INTEGER;
+		token.lexeme = number;
+		return token;
+	}
+
+	public static Token FLOAT(String number, int linePos, int charPos)
+	{
+		Token token = new Token(linePos, charPos);
+		token.kind = Kind.FLOAT;
+		token.lexeme = number;
+		return token;
+	}
+
+
+	private Token(int lineNum, int charPos)
+	{
+		this.lineNum = lineNum;
+		this.charPos = charPos;
+		
+		// if we don't match anything, signal error
+		this.kind = Kind.ERROR;
+		this.lexeme = "No Lexeme Given";
+	}
+	
+	public Token(String lexeme, int lineNum, int charPos)
+	{
+		this.lineNum = lineNum;
+		this.charPos = charPos;
+		
+		// TODO: based on the given lexeme determine and set the actual kind
+		for (Token.Kind kind : Token.Kind.values())
+		{
+			if (kind.matches(lexeme))
+			{
+				this.kind = kind;
+				return;
+			}
+		}
+		
+		// if we don't match anything, signal error
+		this.kind = Kind.ERROR;
+		this.lexeme = "Unrecognized lexeme: " + lexeme;
+	}
+	
+	public int lineNumber()
+	{
+		return lineNum;
+	}
+	
+	public int charPosition()
+	{
+		return charPos;
+	}
+	
+	// Return the lexeme representing or held by this token
+	public String lexeme()
+	{
+		return this.lexeme;
+	}
+	
+	public String toString()
+	{
+		// TODO: implement this
+		String extra = "";
+		if (!kind.hasStaticLexeme())
+			extra = "(" + this.lexeme + ")";
+		return this.kind + extra + "(lineNum:" + this.lineNum + ", charPos:" + this.charPos + ")" ;
+	}
+
+	public boolean is(Token.Kind kind)
+	{
+		return kind == this.kind;
+	}
+	
+	// OPTIONAL: function to query a token about its kind
+	//           boolean is(Token.Kind kind)
+	
+	// OPTIONAL: add any additional helper or convenience methods
+	//           that you find make for a clean design
+
+}
